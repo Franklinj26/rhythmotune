0e60f855fb30b8c11a9c63cd0b094352b8c3219d
Franklin <franklinbun10@gmail.com>
2025-06-19 02:03:42 +0200

    Añadir envio de correo Registro6

--- HOME/php/contacto.php
+++ HOME/php/contacto.php
@@ -5,9 +5,9 @@ session_start();
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../../phpmailer/src/Exception.php';
-require '../../phpmailer/src/PHPMailer.php';
-require '../../phpmailer/src/SMTP.php';
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
 
 final class Contacto {
